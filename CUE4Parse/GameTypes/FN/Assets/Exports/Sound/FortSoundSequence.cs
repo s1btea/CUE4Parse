57916d2ecbc0b3ba929bd0cc6d03d3cdbddcbe21
s1btea <upstore.pw@gmail.com>
2024-08-26 22:08:15 +0700

    umh... yeah, it works exactly like that.

--- CUE4Parse/GameTypes/FN/Assets/Exports/Sound/FortSoundSequence.cs
+++ CUE4Parse/GameTypes/FN/Assets/Exports/Sound/FortSoundSequence.cs
@@ -1,35 +1,17 @@
-using CUE4Parse.UE4.Assets.Exports;
 using CUE4Parse.UE4.Assets.Exports.Sound;
 using CUE4Parse.UE4.Assets.Objects;
-using CUE4Parse.UE4.Assets.Readers;
+using CUE4Parse.UE4.Assets.Utils;
 
 namespace CUE4Parse.GameTypes.FN.Assets.Exports.Sound;
 
-public class UFortSoundSequence : UObject
-{
-    public FSoundSequenceData[] SoundSequenceData;
-    
-    public override void Deserialize(FAssetArchive Ar, long validPos)
-    {
-        base.Deserialize(Ar, validPos);
 
-        if (TryGetValue(out FStructFallback[] soundSequenceData, nameof(SoundSequenceData)))
-        {
-            SoundSequenceData = new FSoundSequenceData[soundSequenceData.Length];
-            for (var i = 0; i < SoundSequenceData.Length; i++)
-            {
-                SoundSequenceData[i] = new FSoundSequenceData(soundSequenceData[i]);
-            }
-        }
-    }
-}
-
-public class FSoundSequenceData
+[StructFallback]
+public class USoundSequenceData
 {
     public USoundCue Sound;
     public float Delay;
 
-    public FSoundSequenceData(FStructFallback fallback)
+    public USoundSequenceData(FStructFallback fallback)
     {
         Sound = fallback.GetOrDefault<USoundCue>(nameof(Sound));
         Delay = fallback.GetOrDefault<float>(nameof(Delay));
